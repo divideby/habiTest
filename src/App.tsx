e6de6e3f64341c59687f7993fd07f15d8f4c8267
Dmitriy <divideby@yandex.ru>
2018-07-11 20:34:26 +0800

    add mapbox example

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import * as React from 'react';
-import './App.css';
+import * as mapboxgl from "mapbox-gl/dist/mapbox-gl";
+import * as React from "react";
 
-import logo from './logo.svg';
+import "./App.css";
+
+(mapboxgl as any).accessToken =
+  "pk.eyJ1IjoiZGl2aWRlYnkiLCJhIjoiY2pqaDNhc2N3MDltdDNwcmw4aDNyMTJmcyJ9.XGhJBeOsXtqQqTTNemDQWA";
 
 class App extends React.Component {
+
+  public componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v10"
+    });
+    
+    map.on("load", () => {
+      // hello
+    });
+  }
+
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <div id="map" />
       </div>
     );
   }
